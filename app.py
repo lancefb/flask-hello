333a430f6f7f9bcbdb028b37ce62bccf5df5d5be
Lance Barker <lancefb@gmail.com>
2023-07-04 23:37:54 -0700

    Update app.py Joke and template

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Barky! How are you?'
+
+def home():
+    response = requests.get('https://icanhazdadjoke.com/', headers={"Accept": "application/json"})
+    data = response.json()
+    return render_template('index.html', joke=data['joke'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
